024de8dc93f9a0f0b262a00bd9bd0f6005a9d314
Claudio Poli <masterkain@gmail.com>
2010-07-14 22:04:12 +0200

    argh

--- lib/rtaglib_mswin32.rb
+++ lib/rtaglib_mswin32.rb
@@ -1,4 +1,4 @@
-if RUBY_RUBY_PLATFORM =~ /mswin32/ || RUBY_RUBY_PLATFORM =~ /mingw32/
+if RUBY_PLATFORM =~ /mswin32/ || RUBY_PLATFORM =~ /mingw32/
   TAGLIB_BASE_PATH = File.expand_path(File.dirname(__FILE__)+"/..")+"/ext/taglib/taglib-mswin32"
   ENV['PATH'] += ";" + (TAGLIB_BASE_PATH + "/bin").gsub("/","\\")
 end
