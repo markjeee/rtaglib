55b7cd591a7c0f40b74ac3bc047f4aeb4fac494f
Claudio Poli <masterkain@gmail.com>
2010-07-14 22:02:35 +0200

    first fix

--- lib/TagLib.rb
+++ lib/TagLib.rb
@@ -1,10 +1,10 @@
 begin
-	require 'rtaglib_mswin32'
-	require 'taglib/TagLib'
+  require 'rtaglib_mswin32'
+  require 'taglib/TagLib'
 rescue LoadError
   $stderr.puts "Warning: taglib/TagLib not found. This is normal if the extension has not beed build yet"
 end
 
 module TagLib
-VERSION="0.3.0"
+  VERSION = "0.3.1"
 end
--- lib/rtaglib_mswin32.rb
+++ lib/rtaglib_mswin32.rb
@@ -1,4 +1,4 @@
-if(PLATFORM=~/mswin32/ or PLATFORM=~/mingw32/ )
-	TAGLIB_BASE_PATH=File.expand_path(File.dirname(__FILE__)+"/..")+"/ext/taglib/taglib-mswin32"
-	ENV['PATH']+=";"+(TAGLIB_BASE_PATH+"/bin").gsub("/","\\")
+if RUBY_PLATFORM =~ /mswin32/ || RUBY_PLATFORM =~ /mingw32/
+  TAGLIB_BASE_PATH = File.expand_path(File.dirname(__FILE__)+"/..")+"/ext/taglib/taglib-mswin32"
+  ENV['PATH'] += ";" + (TAGLIB_BASE_PATH + "/bin").gsub("/","\\")
 end
