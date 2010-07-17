faba35d1cbe592155637d8c6c0ca812317756b64
Claudio Poli <masterkain@gmail.com>
2010-07-18 01:18:41 +0200

    gitignore

--- pkg/rtaglib-0.3.1/lib/tagfile.rb
+++
@@ -1,6 +0,0 @@
-begin
-  require 'rtaglib_mswin32'
-  require 'tagfile/tagfile'
-rescue LoadError
-  $stderr.puts "Warning: tagfile/tagfile not found. This is normal if the extension has not beed build yet"
-end
